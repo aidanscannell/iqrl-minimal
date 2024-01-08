b9fb31b27315c0d955b6599a899aa353b827a432
Aidan Scannell <scannell.aidan@gmail.com>
2024-01-08 19:28:08 +0200

    fix

--- agents/ddpg_ae.py
+++ agents/ddpg_ae.py
@@ -386,7 +386,7 @@ class DDPG_AE(Agent):
 
             # Reset ae/actor/critic after a fixed number of parameter updates
             if self.reset_strategy == "every-x-param-updates":
-                if not isinstance(self.reset_params_freq, int):
+                if isinstance(self.reset_params_freq, int):
                     if self.ddpg.critic_update_counter % self.reset_params_freq == 0:
                         self.reset(full_reset=False)
 
